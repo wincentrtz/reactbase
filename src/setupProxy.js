1e7ad23a87f11c060d527cc2e1fb09df0f0f837c
peterchu999 <peter.andrew987@gmail.com>
2020-04-29 13:49:51 +0700

    changes proxy according to review

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,18 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const env = require("./constants/env/index");
 
-const devProxy = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://localhost:3000",
-      changeOrigin: true,
-      pathRewrite: {
-        "^/api": "",
-      },
-    })
-  );
+const proxy = function (app) {
+  process.env.REACT_APP_STATE === env.DEV_NO_MOCK &&
+    app.use(
+      "/api",
+      createProxyMiddleware({
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+      })
+    );
 };
 
-module.exports =
-  process.env.REACT_APP_STATE === env.DEV_NO_MOCK ? devProxy : () => {};
+module.exports = proxy;
