95f4d3e214fb7ad197663a1ca5c3b23ce1efc338
peterchu999 <peter.andrew987@gmail.com>
2020-07-17 07:52:49 +0700

    refactor test:coverage name & changing reducer payload with mock data

--- src/store/user/__tests__/reducers.test.js
+++ src/store/user/__tests__/reducers.test.js
@@ -1,9 +1,13 @@
 import reducer from "../reducers";
+import mocks from "mocks/modules/users";
+import config from "config";
 import { FETCH_ACTIVE_USER } from "../constants";
+
+const getExpectedResult = (urls) => mocks.find((mock) => mock.url === urls);
 
 const fetchActiveUserAction = {
   type: FETCH_ACTIVE_USER,
-  payload: { status: 200, data: { name: "Wincent" } },
+  payload: getExpectedResult(config.urls.user.getActiveUser),
 };
 
 describe("User Reducers Test", () => {
